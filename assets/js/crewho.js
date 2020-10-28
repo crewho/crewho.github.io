2f5c08ad653a0b629aaab16da4ab1d524b662001
Advay-Inabathini <iadvay@gmail.com>
2020-10-28 10:26:45 +0530

    working testimonial section

--- assets/js/crewho.js
+++ assets/js/crewho.js
@@ -127,3 +127,25 @@ const media = window.matchMedia("screen and (max-width: 600px)");
 //   }
 // }
 ST.enable();
+
+
+var slides = document.querySelectorAll(".fromRight");
+
+var action = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#sec02",
+    pin:true,
+    scrub:0.3,
+    start: "top top",
+    end: "+=3000",
+    // snap: {
+    //     snapTo: "#testimonials", // snap to the closest label in the timeline
+    //     duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+    //     delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+    //     ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
+    //   }
+  }
+})
+.to(slides, {xPercent: -100, duration:5, ease: "none", stagger:3, delay: 0.5})
+// .to({},{duration:1})    // an empty tween to generate a pause at the end
+  
