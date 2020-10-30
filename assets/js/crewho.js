51da77c7ef3fdbc376a7c18bc6f26226d7104f65
Advay-Inabathini <iadvay@gmail.com>
2020-10-30 21:43:24 +0530

    testimonial pause at end

--- assets/js/crewho.js
+++ assets/js/crewho.js
@@ -62,8 +62,6 @@ const ST = ScrollTrigger.create({
   start: "top top",
   end: "bottom bottom",
   onUpdate: getCurrentSection,
-//   start: "-5%",
-//   pin: "#pin-this"
 });
 
 const contentMarkers = gsap.utils.toArray(".contentMarker");
@@ -138,16 +136,10 @@ var action = gsap.timeline({
     scrub:0.3,
     start: "top top",
     end: "+=3000",
-    // snap: {
-    //     snapTo: "#testimonials", // snap to the closest label in the timeline
-    //     duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-    //     delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-    //     ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
-    //   }
   }
 })
 .to(slides, {xPercent: -100, duration:5, ease: "none", stagger:3, delay: 0.5})
-// .to({},{duration:1})    // an empty tween to generate a pause at the end
+.to({},{duration:1})    // an empty tween to generate a pause at the end
   
 
 console.log("Hello there, fellow inspector")
